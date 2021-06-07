7beac64cb9ae125109478339d57203d45286b197
anna-gordiyenko <83456197+anna-gordiyenko@users.noreply.github.com>
2021-06-07 09:35:10 +0300

    first try on functions (#2)
    * first try on functions
    
    * resolved comments
    
    * final version of task1

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (max < min) {
+    const swap = max;
+    max = min;
+    min = swap;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+getRandomIntInclusive(10.5, 58.67);
+
+function getRandomСoordinate(min, max, afterPoint = 2) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (max < min) {
+    const swap = max;
+    max = min;
+    min = swap;
+  }
+  const coordinate = Math.floor(Math.random() * (max - min + 1)) + min;
+  return Number(coordinate.toFixed(afterPoint));
+}
+getRandomСoordinate(59.41504210490353, 18.25022450632139);
