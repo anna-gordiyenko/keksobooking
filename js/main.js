84bf26712843e3e6e31a97a7af4fe1da273d7681
anna-gordiyenko <anna.v.klymenko@gmail.com>
2021-06-09 10:45:21 +0300

    fixed errors

--- js/main.js
+++ js/main.js
@@ -23,9 +23,7 @@ function getRandomСoordinate(min, max, afterPoint = 2) {
 }
 getRandomСoordinate(59.41504210490353, 18.25022450632139);
 
-const getRandomArrayElement = (elements) => {
-  return elements[getRandomIntInclusive(0, elements.length - 1)];
-};
+const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
 
 const SIMILAR_OBJECT_COUNT = 10;
 
@@ -69,11 +67,11 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
-const AUTHOR = {
-  avatar: 'img/avatars/' + getRandomArrayElement(NUMBERS) + '.png',
+const author = {
+  avatar: `img/avatars/${getRandomArrayElement(NUMBERS)}.png`,
 };
 
-const OFFER = {
+const offer = {
   title: 'заголовок',
   address: String(getRandomСoordinate(59.41504210490353, 18.25022450632139)),
   price: getRandomIntInclusive(50, 3500),
@@ -82,22 +80,17 @@ const OFFER = {
   guests: getRandomIntInclusive(1, 500),
   checkin: getRandomArrayElement(CHECK),
   checkout: getRandomArrayElement(CHECK),
-  features: new Array(getRandomIntInclusive(1, 6)).fill(null).map(() => getRandomArrayElement(FEATURES)),
+  features: new Array(getRandomIntInclusive(1, 6)).fill(null).map(() => FEATURES.slice(0, getRandomArrayElement(FEATURES))),
   description: 'описание',
   photos: new Array(getRandomIntInclusive(1, 3)).fill(null).map(() => getRandomArrayElement(PHOTOS)),
 };
 
-const LOCATION = {
+const loc = {
   lat: getRandomСoordinate(35.65000, 35.70000),
   lng: getRandomСoordinate(139.70000, 139.80000),
 };
 
-const createAnnouncement = () => {
-  return {
-    author: '',
-    offer: '',
-    location: '',
-  };
-};
+const createAnnouncement = () => ({ author, offer, loc });
 
 const similarObjects = new Array(SIMILAR_OBJECT_COUNT).fill(null).map(() => createAnnouncement());
+similarObjects;
